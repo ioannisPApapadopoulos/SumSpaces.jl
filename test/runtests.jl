f2bbd77c6b99f57850b5f6ea81bf107311f9153c
ioannisPApapadopoulos <john.papadopoulos0@gmail.com>
2022-06-15 16:35:46 +0100

    add packages to test deps

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using Test, SumSpaces, ClassicalOrthogonalPolynomials
 import ClassicalOrthogonalPolynomials: sqrtx2
 
 include("test_extendedchebyshev.jl")
+include("test_sumspace.jl")
---
+++ test/test_sumspace.jl
@@ -0,0 +1,10 @@
+using Test, SumSpaces, ClassicalOrthogonalPolynomials
+
+"""
+Test functions in sumspace.jl
+"""
+
+@testset "sumspace" begin
+
+
+end
