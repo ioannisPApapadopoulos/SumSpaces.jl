69276404432f0350a1629d92760e75320a35c66d
ioannisPApapadopoulos <john.papadopoulos0@gmail.com>
2022-06-29 21:49:24 +0100

    Merge frames framework

---
+++ src/frame.jl
@@ -0,0 +1,111 @@
+# Custom SVD solver for least squares problems
+function solvesvd(A::AbstractArray, b::AbstractVector; tol::Real=1e-7)
+    Am = A[1:end,1:end] # BlockBandedMatrices cannot do SVD
+    U,S,V = svd(Am)
+    S⁺ = inv.(S)[:]
+    S⁺[S⁺ .> 1/tol] .= 0
+    c = V * Diagonal(S⁺) * U' * b
+    return c
+end
+
+# Affine transformation
+at(a,b,x) = (b-a)/2 * x .+ (b+a)/2
+
+# Construct collocation points
+function collocation_points(M::Int, Me::Int; a::AbstractVector=[-1.,1.], endpoints::AbstractVector=[-5.,5.], innergap::Real = 0.)
+    Tp = eltype(a)
+    el_no = length(a)-1
+
+    x = Array{Tp}(undef,el_no*M+2*Me)
+    xnodes = LinRange{Tp}(innergap,1-innergap,M)
+    # chebnodes = sort(cos.(π.*xnodes))
+
+    xxnodes = LinRange{Tp}(-1+innergap,1-innergap,M)
+    for el = 1:el_no
+        x[(el-1)*M+1:el*M] = at(a[el], a[el+1], xxnodes) 
+    end
+    xnodes = LinRange{Tp}(innergap,1-innergap,Me)
+    # chebnodes = sort(cos.(π.*xnodes))
+
+    xxnodes = LinRange{Tp}(-1+innergap,1-innergap,Me)
+    x[el_no*M+1:el_no*M+Me] = at(endpoints[1], a[1], xxnodes) 
+    x[el_no*M+1+Me:el_no*M+2*Me] = at(a[end],endpoints[2],xxnodes)
+    return sort(unique(x))
+end
+
+# Convert function evaluation to Riemann sum
+function riemann(x::AbstractVector, f::Function)
+    y = sort(x)
+    h = 0.5 .* (
+            append!(y[2:end], y[end]) .- y
+         .+ y .- append!([y[1]], y[1:end-1])
+    )
+    b = sqrt.(h).*f(y)
+    return b
+end
+
+# Just function evaluation
+function evaluate(x::AbstractVector, f::Function)
+    y = sort(x)
+    b = f(y)
+    return b
+end
+
+# Fit low order expansion to higher order expansion
+function expansion_sum_space(c::AbstractVector, N::Int, el_no::Int)
+    v = zeros(1 + el_no*(N-1))
+    v[1:length(c)] = c
+    return v
+end
+
+# Construct Least Squares matrix for frame coefficient computation
+
+function framematrix(x::AbstractVector, Sp::SumSpaceP, Nn::Int; normtype::Function=riemann)
+    Tp = eltype(Sp)
+    el = length(Sp.I) - 1
+    rows = [length(x)]; cols = vcat([1], Fill(2, el*(Nn+1)))
+    # Create correct block structure
+    A = BlockBandedMatrix(Zeros(sum(rows),sum(cols)), rows, cols, (sum(rows),sum(cols)))
+    A[:,Block.(1:length(cols))] = normtype(x, x->Sp[x, Block.(1:length(cols))])
+    return A
+end
+
+function framematrix(x::AbstractVector, Sp::ElementSumSpaceP, Nn::Int; normtype::Function=riemann)
+    Tp = eltype(Sp)
+    el = length(Sp.I) - 1
+    rows = [length(x)]; cols = vcat([1], Fill(el, (2*Nn+2)))
+
+    # Create correct block structure
+    A = BlockBandedMatrix(Zeros(sum(rows),sum(cols)), rows, cols, (sum(rows),sum(cols)))
+    
+    # Form columns of Least Squares matrix.
+    A[:,Block.(1:length(cols))] = normtype(x, x->Sp[x, Block.(1:length(cols))])
+    return A
+end
+
+# Construct Least Squares matrix for dual sum space
+function framematrix(x::AbstractVector, Sd::SumSpaceD, Nn::Int; normtype::Function=riemann)
+    Tp = eltype(Sd)
+    el = length(Sd.I) - 1
+ 
+    rows = [length(x)]; cols = vcat([1], Fill(2, el*(Nn+3)))
+  
+    # Create correct block structure
+    A = BlockBandedMatrix(Zeros(sum(rows),sum(cols)), rows, cols, (sum(rows),sum(cols)))
+    
+    # Form columns of Least Squares matrix.
+    A[:,Block.(1:length(cols))] = normtype(x, x->Sd[x, Block.(1:length(cols))])
+    return A
+end
+
+function framematrix(x::AbstractVector, Sd::ElementSumSpaceD, Nn::Int; normtype::Function=riemann)
+    Tp = eltype(Sd)
+    el = length(Sd.I) - 1
+    rows = [length(x)]; cols = vcat([1], Fill(el, (2*Nn+6)))
+    # Create correct block structure
+    A = BlockBandedMatrix(Zeros(sum(rows),sum(cols)), rows, cols, (sum(rows),sum(cols)))
+    
+    # Form columns of Least Squares matrix.
+    A[:,Block.(1:length(cols))] = normtype(x, x->Sd[x, Block.(1:length(cols))])
+    return A
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,4 @@ import ClassicalOrthogonalPolynomials: sqrtx2
 include("test_extendedchebyshev.jl")
 include("test_sumspace.jl")
 include("test_elementsumspace.jl")
+include("test_frame.jl")
